2ace78e111115be6c777c77d66a4c7043824f55b
spypunk <adrien.spanier@gmail.com>
2016-07-26 20:36:38 +0200

    Formatting

--- src/main/java/spypunk/tetris/controller/TetrisControllerImpl.java
+++ src/main/java/spypunk/tetris/controller/TetrisControllerImpl.java
@@ -21,75 +21,75 @@ import spypunk.tetris.view.TetrisRenderer;
 @Singleton
 public class TetrisControllerImpl implements TetrisController {
 
-	private static final int RENDER_PERIOD = 1000 / 60;
+    private static final int RENDER_PERIOD = 1000 / 60;
 
-	private static final Map<Integer, Movement> MOVEMENTS = Maps.newHashMap();
+    private static final Map<Integer, Movement> MOVEMENTS = Maps.newHashMap();
 
-	static {
-		MOVEMENTS.put(KeyEvent.VK_DOWN, Movement.DOWN);
-		MOVEMENTS.put(KeyEvent.VK_RIGHT, Movement.RIGHT);
-		MOVEMENTS.put(KeyEvent.VK_LEFT, Movement.LEFT);
-		MOVEMENTS.put(KeyEvent.VK_UP, Movement.ROTATE_CW);
-	}
+    static {
+        MOVEMENTS.put(KeyEvent.VK_DOWN, Movement.DOWN);
+        MOVEMENTS.put(KeyEvent.VK_RIGHT, Movement.RIGHT);
+        MOVEMENTS.put(KeyEvent.VK_LEFT, Movement.LEFT);
+        MOVEMENTS.put(KeyEvent.VK_UP, Movement.ROTATE_CW);
+    }
 
-	@Inject
-	private ScheduledExecutorService scheduledExecutorService;
+    @Inject
+    private ScheduledExecutorService scheduledExecutorService;
 
-	@Inject
-	private TetrisRenderer tetrisRenderer;
+    @Inject
+    private TetrisRenderer tetrisRenderer;
 
-	@Inject
-	private TetrisService tetrisService;
+    @Inject
+    private TetrisService tetrisService;
 
-	@Inject
-	private TetrisFactory tetrisFactory;
+    @Inject
+    private TetrisFactory tetrisFactory;
 
-	private volatile boolean newGame = true;
+    private volatile boolean newGame = true;
 
-	private volatile Movement movement;
+    private volatile Movement movement;
 
-	private Future<?> loopThread;
+    private Future<?> loopThread;
 
-	private Tetris tetris;
+    private Tetris tetris;
 
-	@Override
-	public void start() {
-		tetrisRenderer.start();
+    @Override
+    public void start() {
+        tetrisRenderer.start();
 
-		loopThread = scheduledExecutorService.scheduleAtFixedRate(() -> onGameLoop(), 0, RENDER_PERIOD,
-				TimeUnit.MILLISECONDS);
-	}
+        loopThread = scheduledExecutorService.scheduleAtFixedRate(() -> onGameLoop(), 0, RENDER_PERIOD,
+                TimeUnit.MILLISECONDS);
+    }
 
-	@Override
-	public void onWindowClosed() {
-		loopThread.cancel(false);
-		scheduledExecutorService.shutdown();
-	}
+    @Override
+    public void onWindowClosed() {
+        loopThread.cancel(false);
+        scheduledExecutorService.shutdown();
+    }
 
-	@Override
-	public void onKeyPressed(int keyCode) {
-		if (KeyEvent.VK_SPACE == keyCode) {
-			newGame = true;
-			return;
-		}
+    @Override
+    public void onKeyPressed(int keyCode) {
+        if (KeyEvent.VK_SPACE == keyCode) {
+            newGame = true;
+            return;
+        }
 
-		movement = MOVEMENTS.get(keyCode);
-	}
+        movement = MOVEMENTS.get(keyCode);
+    }
 
-	private void onGameLoop() {
-		if (newGame) {
-			tetris = tetrisFactory.createTetris();
-			newGame = false;
-		} else if (tetris == null) {
-			return;
-		} else {
-			tetris.setMovement(Optional.ofNullable(movement));
+    private void onGameLoop() {
+        if (newGame) {
+            tetris = tetrisFactory.createTetris();
+            newGame = false;
+        } else if (tetris == null) {
+            return;
+        } else {
+            tetris.setMovement(Optional.ofNullable(movement));
 
-			movement = null;
+            movement = null;
 
-			tetrisService.update(tetris);
-		}
+            tetrisService.update(tetris);
+        }
 
-		tetrisRenderer.render(tetris);
-	}
+        tetrisRenderer.render(tetris);
+    }
 }
