2cd63358d2fa7b33162b2ddbc529752f06381fc1
spypunk <adrien.spanier@gmail.com>
2016-08-12 17:17:19 +0200

    Useless code removed

--- src/main/java/spypunk/tetris/ui/service/MusicServiceImpl.java
+++ src/main/java/spypunk/tetris/ui/service/MusicServiceImpl.java
@@ -13,53 +13,18 @@ import javax.inject.Singleton;
 @Singleton
 public class MusicServiceImpl implements MusicService {
 
-    private enum State {
-        PLAYING {
-            @Override
-            public State pause() {
-                return PAUSED;
-            }
-        },
-        PAUSED {
-            @Override
-            public State pause() {
-                return PLAYING;
-            }
-        },
-        STOPPED {
-            @Override
-            public State play() {
-                return PLAYING;
-            }
-        };
-
-        public State play() {
-            return this;
-        }
-
-        public State pause() {
-            return this;
-        }
-
-        public State stop() {
-            return STOPPED;
-        }
-    }
-
-    private State state = State.STOPPED;
-
     @Override
     public void playMusic() {
-        state = state.play();
+        // TODO
     }
 
     @Override
     public void pauseMusic() {
-        state = state.pause();
+        // TODO
     }
 
     @Override
     public void stopMusic() {
-        state = state.stop();
+        // TODO
     }
 }
