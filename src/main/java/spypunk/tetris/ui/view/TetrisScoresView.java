a849ff575b44a7728f13dcf6b8fb7890d1b7ad08
Bugra <abugraokkali@outlook.com>
2021-03-31 19:56:08 +0300

    TetrisScoresView is created for high-score window.

---
+++ src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
@@ -0,0 +1,18 @@
+package spypunk.tetris.ui.view;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class TetrisScoresView {
+    JFrame frame;
+    public TetrisScoresView(){
+        frame=new JFrame("High scores");
+    }
+    public void show(){
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+    }
+}
