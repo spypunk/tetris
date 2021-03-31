2ad25e25bcaa85441db35da4eece6f0249a844d4
Bugra <abugraokkali@outlook.com>
2021-03-31 20:50:06 +0300

    A test label added to score window.

--- src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
+++ src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
@@ -3,17 +3,28 @@ package spypunk.tetris.ui.view;
 import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.Dimension;
+import javax.swing.JLabel;
 
-public class TetrisScoresView {
+
+public class TetrisScoresView{
+
     JFrame frame;
+
     public TetrisScoresView(){
         frame=new JFrame("High scores");
-        frame.getContentPane().setBackground(Color.BLACK);
+        frame.getContentPane().setBackground(Color.WHITE);
         frame.setPreferredSize(new Dimension(400, 300));
         frame.pack();
         frame.setLocationRelativeTo(null);
+        
     }
     public void show(){
+        print();
         frame.setVisible(true);
+    }
+    public void print(){
+        final JLabel firstLabel = new JLabel("Test");
+        firstLabel.setForeground(Color.BLACK);
+        frame.add(firstLabel);
     }
 }
