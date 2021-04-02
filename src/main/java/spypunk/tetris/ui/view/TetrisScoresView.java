2499da68b1c82df3fa53fc78512c65ad660a40db
Bugra <abugraokkali@outlook.com>
2021-04-03 00:52:23 +0300

    Score table reader method is added.

--- src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
+++ src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
@@ -3,6 +3,11 @@ package spypunk.tetris.ui.view;
 import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.JLabel;
 
 import static spypunk.tetris.ui.constants.TetrisUIConstants.DEFAULT_FONT_COLOR;
@@ -10,12 +15,14 @@ import static spypunk.tetris.ui.constants.TetrisUIConstants.DEFAULT_FONT_COLOR;
 public class TetrisScoresView{
 
     JFrame frame;
+    ArrayList<String> lines;
     public TetrisScoresView(){
         frame=new JFrame("High scores");
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setPreferredSize(new Dimension(400, 300));
         frame.pack();
         frame.setLocationRelativeTo(null);
+        lines=new ArrayList<>();
         
     }
     public void show(){
@@ -26,5 +33,19 @@ public class TetrisScoresView{
         final JLabel firstLabel = new JLabel("Test");
         firstLabel.setForeground(DEFAULT_FONT_COLOR);
         frame.add(firstLabel);
+    }
+    public void readScores(){
+        try {
+            File myObj = new File("score_table.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              lines.add(data);
+            }
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
     }
 }
