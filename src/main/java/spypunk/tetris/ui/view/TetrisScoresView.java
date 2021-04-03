fcde1c683526c8455661fdc49e7ee9f755649480
Bugra <abugraokkali@outlook.com>
2021-04-03 23:56:53 +0300

    Some changes in the High-Score window.

--- src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
+++ src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
@@ -2,28 +2,38 @@ package spypunk.tetris.ui.view;
 
 import javax.swing.JFrame;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
 
 import static spypunk.tetris.ui.constants.TetrisUIConstants.DEFAULT_FONT_COLOR;
+import spypunk.tetris.ui.font.cache.FontCacheImpl;
 
 public class TetrisScoresView{
 
     JFrame frame;
     HashMap<String,Integer> lines;
+    Container contentPane;
+
     public TetrisScoresView(){
+        lines=new HashMap<>();
+
         frame=new JFrame("High scores");
-        frame.getContentPane().setBackground(Color.BLACK);
-        frame.setPreferredSize(new Dimension(400, 300));
+        contentPane=frame.getContentPane();
+
+        contentPane.setBackground(Color.BLACK);
+        frame.setPreferredSize(new Dimension(300, 300));
+        frame.setResizable(false);
         frame.pack();
         frame.setLocationRelativeTo(null);
-        lines=new HashMap<>();
+        
         readScores();
         
     }
@@ -32,9 +42,26 @@ public class TetrisScoresView{
         frame.setVisible(true);
     }
     public void print(){
-        final JLabel firstLabel = new JLabel("Test");
-        firstLabel.setForeground(DEFAULT_FONT_COLOR);
-        frame.add(firstLabel);
+        int counter=1;
+        String s="";
+        for ( String key : lines.keySet() ) {
+            s+="\n  "+counter+") "+key+" "+lines.get(key);
+            counter++;
+        }
+        System.out.println(s);
+        final JTextArea textArea = new JTextArea(s);
+        textArea.setBackground(Color.BLACK);
+        textArea.setForeground(DEFAULT_FONT_COLOR);
+        textArea.setEditable(false);
+        
+        FontCacheImpl obj=new FontCacheImpl();
+        
+        Font font=obj.getDefaultFont();
+        float size=font.getSize()+8.0f;
+        textArea.setFont(font.deriveFont(size));
+        frame.add(textArea);
+        
+        
     }
     public void readScores(){
         try {
