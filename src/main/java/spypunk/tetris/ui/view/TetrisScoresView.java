0a84c043fbef54b110b18be56456c5b1e60b584c
Bugra <abugraokkali@outlook.com>
2021-04-04 00:56:46 +0300

    Sort method added.

--- src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
+++ src/main/java/spypunk/tetris/ui/view/TetrisScoresView.java
@@ -7,9 +7,14 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-
 import javax.swing.JTextArea;
 
 
@@ -19,11 +24,11 @@ import spypunk.tetris.ui.font.cache.FontCacheImpl;
 public class TetrisScoresView{
 
     JFrame frame;
-    HashMap<String,Integer> lines;
+    HashMap<String,Integer> hm;
     Container contentPane;
 
     public TetrisScoresView(){
-        lines=new HashMap<>();
+        hm=new HashMap<>();
 
         frame=new JFrame("High scores");
         contentPane=frame.getContentPane();
@@ -42,13 +47,13 @@ public class TetrisScoresView{
         frame.setVisible(true);
     }
     public void print(){
+        hm=sort(hm);
         int counter=1;
         String s="";
-        for ( String key : lines.keySet() ) {
-            s+="\n  "+counter+") "+key+" "+lines.get(key);
+        for ( String key : hm.keySet() ) {
+            s+="\n  "+counter+") "+key+" "+hm.get(key);
             counter++;
         }
-        System.out.println(s);
         final JTextArea textArea = new JTextArea(s);
         textArea.setBackground(Color.BLACK);
         textArea.setForeground(DEFAULT_FONT_COLOR);
@@ -72,12 +77,27 @@ public class TetrisScoresView{
               String name=data.substring(0,data.indexOf(" "));
               String score_s=data.substring(data.indexOf(" ")+1);
               Integer score=Integer.parseInt(score_s);
-              lines.put(name, score);
+              hm.put(name, score);
             }
             myReader.close();
           } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
           }
+    }
+    public HashMap<String,Integer> sort(HashMap<String,Integer> hashMap){
+        List<Map.Entry<String,Integer>>list=new LinkedList<Map.Entry<String,Integer>>(hashMap.entrySet());
+        Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){
+                
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+        });
+        HashMap<String,Integer> temp=new LinkedHashMap<>();
+        for(Map.Entry<String,Integer > aa:list){
+            temp.put(aa.getKey(),aa.getValue());
+        }
+        return temp;
     }
 }
